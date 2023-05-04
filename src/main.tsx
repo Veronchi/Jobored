c83025796e19497f51a9f446570a2ae3624489e7
Veronchi <zhigunova1995@gmail.com>
2023-05-04 21:14:24 +0300

    feat: add common styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
+import './styles/reset.scss';
+import './styles/normalize.scss';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
